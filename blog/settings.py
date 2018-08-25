adb292f7e26553d8e03cb71f0088715f24bf45da
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-08-25 17:20:52 +0100

    Cleanup unused constant

--- blog/settings.py
+++ blog/settings.py
@@ -9,8 +9,6 @@ APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = parent_dir(APP_DIR)
 # In order to deploy to Github pages, you must build the static files to the project root
 FREEZER_DESTINATION = PROJECT_ROOT
-# This is a repo page(not a GH user page). We need to set the BASE_URL to the correct url as per GH Pages' standards
-# FREEZER_BASE_URL = "http://localhost/{0}".format("vasiuadriana.github.io/")
 # IMPORTANT: If this is True, all app files will be deleted when you run the freezer
 FREEZER_REMOVE_EXTRA_FILES = False
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
