9a8771894ba63b1ffc65cdfde821f85e7ca40b0f
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-08-25 17:11:52 +0100

    Test different path to static files in a deployed environment

--- blog/settings.py
+++ blog/settings.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-
 import os
-
-REPO_NAME = "vasiuadriana.github.io/"  # Used for FREEZER_BASE_URL
-DEBUG = True
-
-# Assumes the app is located in the same directory
-# where this file resides
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def parent_dir(path):
-    '''Return the parent of a directory.'''
     return os.path.abspath(os.path.join(path, os.pardir))
 
 
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = parent_dir(APP_DIR)
-# In order to deploy to Github pages, you must build the static files to
-# the project root
+# In order to deploy to Github pages, you must build the static files to the project root
 FREEZER_DESTINATION = PROJECT_ROOT
-# Since this is a repo page (not a Github user page),
-# we need to set the BASE_URL to the correct url as per GH Pages' standards
-FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
-FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
-                                    # will be deleted when you run the freezer
+# This is a repo page(not a GH user page). We need to set the BASE_URL to the correct url as per GH Pages' standards
+# FREEZER_BASE_URL = "http://localhost/{0}".format("vasiuadriana.github.io/")
+# IMPORTANT: If this is True, all app files will be deleted when you run the freezer
+FREEZER_REMOVE_EXTRA_FILES = False
 FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
 FLATPAGES_EXTENSION = '.md'
+
+DEBUG = True
