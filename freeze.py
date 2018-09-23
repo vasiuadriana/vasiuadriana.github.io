917007e1abbe3880c5b22d3bc4ce831966861732
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-09-23 20:32:12 +0100

    Add sitemap

--- freeze.py
+++ freeze.py
@@ -1,4 +1,16 @@
+import os
 from blog.app import app_freezer
+
+
+def process_sitemap():
+    os.rename('sitemap.xml', 'sitemap_unprocessed.xml')
+    with open("sitemap_unprocessed.xml", "rt") as file_in:
+        with open("sitemap.xml", "wt") as file_out:
+            for line in file_in:
+                file_out.write(line.replace('localhost', 'moosesloveforest.com'))
+    os.remove('sitemap_unprocessed.xml')
+
 
 if __name__ == '__main__':
     app_freezer.freeze()
+    process_sitemap()
